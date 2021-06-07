f418a27686ddd9572087dc399067a8d257c478a4
Peter Racine Jr <peterracinejr@gmail.com>
2021-06-07 13:32:00 -0500

    added click event and counter to employees.js

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -2,17 +2,28 @@ import { getEmployees, getOrders } from "./database.js";
 const orders = getOrders();
 
 document.addEventListener("click", (clickEvent) => {
+  // debugger;
   const itemClicked = clickEvent.target;
   if (itemClicked.id.startsWith("employee")) {
     const [, employeeId] = itemClicked.id.split("--");
 
+    const employeeIdNumber = parseInt(employeeId);
+
+    let counter = 0;
+    for (const order of orders) {
+      if (order.employeeId === employeeIdNumber) {
+        counter++;
+      }
+    }
+
     for (const employee of employees) {
-      if (employee.id === parseInt(employeeId)) {
-        window.alert(`${employee.name} sold ${orders.employeeId} products.`);
+      if (employee.id === employeeIdNumber) {
+        window.alert(`${employee.name} sold ${counter} products.`);
       }
     }
   }
 });
+
 const employees = getEmployees();
 
 export const Employees = () => {
